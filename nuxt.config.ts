7aee455758e805c6c93c02944ebfd4e6f5564813
Isael Gomes <otakustudio188@gmail.com>
2023-08-13 11:02:35 -0300

    remove nuxt pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({  
-  modules: ['@vite-pwa/nuxt', '@nuxtjs/tailwindcss'],
+  // modules: ['@vite-pwa/nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss'],
   css: [
     '@/assets/css/main.css',
   ],
-  pwa: {
-    manifest: {
-      name: "WareZap",
-      short_name: "WareZap",
-      description: "Gerenciamento Inteligente",
-      icons: [
-        {
-          src: "icons/icon_64x64.png",
-          sizes: "64x64",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon_144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon_192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon_512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
-    // workbox: {
-    //   navigateFallback: '/'
-    // },
-    devOptions:{
-      enabled: true,
-      type: 'module'
-    }
-  },
+  // pwa: {
+  //   manifest: {
+  //     name: "WareZap",
+  //     short_name: "WareZap",
+  //     description: "Gerenciamento Inteligente",
+  //     icons: [
+  //       {
+  //         src: "icons/icon_64x64.png",
+  //         sizes: "64x64",
+  //         type: "image/png",
+  //       },
+  //       {
+  //         src: "icons/icon_144x144.png",
+  //         sizes: "144x144",
+  //         type: "image/png",
+  //       },
+  //       {
+  //         src: "icons/icon_192x192.png",
+  //         sizes: "192x192",
+  //         type: "image/png",
+  //       },
+  //       {
+  //         src: "icons/icon_512x512.png",
+  //         sizes: "512x512",
+  //         type: "image/png",
+  //       },
+  //     ],
+  //   },
+  //   // workbox: {
+  //   //   navigateFallback: '/'
+  //   // },
+  //   devOptions:{
+  //     enabled: true,
+  //     type: 'module'
+  //   }
+  // },
   ssr: false
 })
