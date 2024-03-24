62169aec2b8c68d930a46397ce6eab53c9adc60b
Isael Gomes <otakustudio188@gmail.com>
2024-03-24 10:20:03 -0300

    new version nuxt, and reset all layout for new struture

--- layouts/blank.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="bg-slate-50/80 h-screen w-screen">        
-        <NuxtPage />
-    </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({  
-  modules: ['@vite-pwa/nuxt', '@nuxtjs/tailwindcss'],  
-  css: [
-    '@/assets/css/main.css',
-  ],
-  pwa: {
-    manifest: {
-      name: "WareZap",
-      short_name: "WareZap",
-      description: "Gerenciamento Inteligente",
-      icons: [
-        {
-          src: "icons/icon_64x64.png",
-          sizes: "64x64",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon_144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon_192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon_512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
-    // workbox: {
-    //   navigateFallback: '/'
-    // },
-    devOptions:{
-      enabled: true,
-      type: 'module'
-    }
-  },
-  ssr: false
+export default defineNuxtConfig({
+  devtools: { enabled: true }
 })
