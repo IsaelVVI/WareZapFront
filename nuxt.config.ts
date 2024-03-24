f2fabcbc29426d0ed15ff3b94e99aaa81ea921cd
Isael Gomes <otakustudio188@gmail.com>
2024-03-24 10:46:36 -0300

    new dependencies and new library for components, shadcnui vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt','@vueuse/nuxt', '@nuxtjs/device', 'shadcn-nuxt'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  runtimeConfig: {
+    public: {
+      base_url: ''
+    }
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
 })
